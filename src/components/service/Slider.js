ff05274d10c3249a5f3dbcd30e4e153fb12a3b2a
Sekai58 <amritachi58@gmail.com>
2024-02-12 11:44:54 +0545

    minor changes

--- src/components/service/Slider.js
+++ src/components/service/Slider.js
@@ -2,7 +2,8 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import SliderCard from "./SliderCard";
 
 const galleryImages = [
   "https://faradayshielding.com.au/wp-content/uploads/2021/05/Image_039.jpg",
@@ -14,7 +15,7 @@ var settings = {
   dots: false,
   centerMode: true,
   infinite: true,
-  slidesToShow: 1,
+  slidesToShow: 2,
   autoplay: true,
   speed: 2000,
   cssEase: "linear",
@@ -22,21 +23,27 @@ var settings = {
   autoplaySpeed: 2000,
   pauseOnHover: false,
   pauseOnFocus: false,
+  responsive: [
+    {
+      breakpoint: 1250,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
 };
 
 const ImageSlider = () => {
   return (
     <section className="px-2 md:px-4 xl:px-container mb-[8rem] flex justify-center ">
-      <div className=" max-w-[80vw] md:max-w-[50vw] relative  bg-gradient-to-r  from-[#44ff9b31] via-[#8c44ff2e] to-[#ecff7033] ">
+      <div className=" max-w-[80vw] md:max-w-[50vw]  bg-gradient-to-tr  from-[#44ff9b31] via-[#8c44ff2e] to-[#ecff7047] ">
         <div className="w-full  bg-white/30 backdrop-blur-xl backdrop-brightness-110 py-10 ">
           <Slider {...settings}>
             {galleryImages.map((data, idx) => {
               return (
-                <div
-                  key={idx}
-                  className={`w-full h-[14rem] xl:h-[18rem]  text-white`}
-                >
-                  <Link to="#">
+                <div key={idx} className={`w-full h-[14rem] xl:h-[16rem]`}>
+                  <SliderCard />
+                  {/* <Link to="#">
                     <div className="px-[3rem] py-[5rem] flex items-center max-h-[20rem] ">
                       <img
                         src={data}
@@ -44,7 +51,7 @@ const ImageSlider = () => {
                         alt="banner"
                       />
                     </div>
-                  </Link>
+                  </Link> */}
                 </div>
               );
             })}
---
+++ src/components/service/SliderCard.js
@@ -0,0 +1,12 @@
+const SliderCard = () => {
+  return (
+    <div className="px-[1rem] md:px-[1.5rem] xl:px-[3rem] py-[1rem] flex items-center h-full ">
+      <div className="rounded-xl px-[3rem] py-8 flex flex-col justify-between bg-white shadow-lg text-grayLine h-full">
+        <p>"Your contacts list is your most important social networking."</p>
+        <p className="uppercase font-medium text-lg">slate</p>
+      </div>
+    </div>
+  );
+};
+
+export default SliderCard;
